bd1362f0040b9dd67ec99e00cc03c028163cc030
fyodorrrrr <aquinojxse@gmail.com>
2025-12-13 03:36:15 +0800

    style: reorder import statements for consistency in serializers

--- backend/apps/members/serializers.py
+++ backend/apps/members/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Member, FamilyMember
+from .models import FamilyMember, Member
 
 
 class FamilyMemberSerializer(serializers.ModelSerializer):
